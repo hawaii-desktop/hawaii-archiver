d5a2d78d0cd444d031d0a4fcde391b80f568e3f0
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-12-03 21:47:42 +0100

    Change license to GPL v2 or, at your option, any later version.

--- src/archivemodel.h
+++ src/archivemodel.h
@@ -1,20 +1,20 @@
 /****************************************************************************
  * This file is part of Archiver.
  *
- * Copyright (c) 2007 Henrique Pinto
- * Copyright (c) 2008-2009 Harald Hvaal
- * Copyright (c) 2012 Pier Luigi Fiorini
+ * Copyright (C) 2012 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ * Copyright (C) 2008-2009 Harald Hvaal <haraldhv@stud.ntnu.no>
+ * Copyright (C) 2007 Henrique Pinto <henrique.pinto@kdemail.net>
  *
  * Author(s):
- *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
- *    Harald Hvaal <haraldhv@stud.ntnu.no>
- *    Henrique Pinto <henrique.pinto@kdemail.net>
+ *    Pier Luigi Fiorini
+ *    Harald Hvaal
+ *    Henrique Pinto
  *
- * $BEGIN_LICENSE:GPL3+$
+ * $BEGIN_LICENSE:GPL2+$
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
